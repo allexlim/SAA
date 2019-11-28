85baebd5d31d0f1325154f79ec8f4712ffc3036c
allexlim <57930167+allexlim@users.noreply.github.com>
2019-11-28 19:44:33 +0700

    Add files via upload

---
+++ SAA AP/Form1.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(493, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(235, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Silahkan di random!!!"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
+        Me.Button1.Location = New System.Drawing.Point(562, 106)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(90, 52)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Random"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Snap ITC", 21.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Strikeout), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(545, 188)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(131, 37)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Label2"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 50
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 50
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+End Class
---
+++ SAA AP/Form1.vb
@@ -0,0 +1,81 @@
+﻿Public Class Form1
+    Dim button(100) As Button
+    Dim game As Button
+    Dim a As Integer = 0
+    Dim wow As Integer = 0
+    Dim angka As Integer = 30
+    Dim hello As Integer = 0
+    Dim simpan As Integer
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        game = New Button
+        game.BackColor = Color.Gold
+        game.Height = 25
+        game.Width = 25
+        game.Left = 0
+        game.Top = 350
+        Me.Controls.Add(game)
+
+
+        For i = 0 To 7
+            For j = 0 To 7
+                If i Mod 2 = 0 Then
+                    a += 1
+                    button(a) = New Button
+                    button(a).Height = 50
+                    button(a).Width = 50
+                    button(a).Left = j * 50
+                    button(a).Top = (7 - i) * 50
+                    button(a).Name = a
+                    button(a).Text = a
+                    Me.Controls.Add(button(a))
+                Else
+                    a += 1
+                    button(a) = New Button
+                    button(a).Height = 50
+                    button(a).Width = 50
+                    button(a).Left = (7 - j) * 50
+                    button(a).Top = (7 - i) * 50
+                    button(a).Name = a
+                    button(a).Text = a
+                    Me.Controls.Add(button(a))
+                End If
+            Next
+        Next
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Randomize()
+        If angka > 0 Then
+            simpan = Rnd() * 5 + 1
+            Label2.Text = simpan
+            angka -= 1
+        Else
+            Timer1.Enabled = False
+            hello = 1
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If hello = 1 Then
+            Timer2.Enabled = False
+            simpan = Rnd() * 5 + 1
+            Label2.Text = simpan
+            Label2.Font = New Font(Label2.Font, FontStyle.Underline)
+            angka = 30
+            hello = 0
+            game.Height = 25
+            game.Width = 25
+            game.Left = simpan + 50
+            game.Top = 350
+            Me.Controls.Add(game)
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Enabled = True
+        Timer2.Enabled = True
+        Label2.Font = New Font(Label2.Font, FontStyle.Strikeout)
+    End Sub
+End Class
